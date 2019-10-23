68dcf58d0a1b3265e5dc4faf28b7cc27856393c4
Evan Pikulski <Evan.Pikulski@ntrepidcorp.com>
2019-10-23 08:40:59 -0400

    Fix versioning.

--- digitalarchive/__init__.py
+++ digitalarchive/__init__.py
@@ -2,7 +2,7 @@
 A python API and ORM for the Wilson Center's Digital Archive of historical documents.
 """
 # __init__.py
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 # Import DA models for convenience.
 from .models import (
