e9d1317b620414322809f55e89319e35c4350c92
Evan Pikulski <epikulski@gmail.com>
2019-10-22 21:23:54 -0400

    Add version to init

--- digitalarchive/__init__.py
+++ digitalarchive/__init__.py
@@ -1,6 +1,10 @@
 """
 A python API and ORM for the Wilson Center's Digital Archive of historical documents.
 """
+# __init__.py
+__version__ = "0.1.0"
+
+# Import DA models for convenience.
 from .models import (
     Subject,
     Language,
