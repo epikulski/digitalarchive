8986640b3f25d4362184b0a78e4c58bfd0886422
Evan Pikulski <28378019+epikulski@users.noreply.github.com>
2019-10-28 20:33:08 -0400

    Version bump to v0.1.4 (#13)

--- digitalarchive/__init__.py
+++ digitalarchive/__init__.py
@@ -2,7 +2,7 @@
 A python API and ORM for the Wilson Center's Digital Archive of historical documents.
 """
 # __init__.py
-__version__ = "0.1.3"
+__version__ = "0.1.4"
 
 # Import DA models for convenience.
 from .models import (
