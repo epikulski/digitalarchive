4e821d441623fd0b13566abb6ddf5f308402066a
Evan Pikulski <28378019+epikulski@users.noreply.github.com>
2019-10-30 19:33:54 -0400

    Bump Version (#18)

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = "https://github.com/epikulski/digitalarchive"
 EMAIL = "epikulsk@gmail.com"
 AUTHOR = "Evan Pikulski"
 REQUIRES_PYTHON = ">=3.7.0"
-VERSION = "0.1.4"
+VERSION = "0.1.5"
 
 # Required 3rd party libraries.
 REQUIRED = ["requests"]
