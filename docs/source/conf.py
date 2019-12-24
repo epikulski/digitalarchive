81a0e3176d3224d15be571f5942f575906fb41e7
Evan Pikulski <epikulski@gmail.com>
2019-12-24 16:09:41 +0100

    Bump version.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = '2019, Evan Pikulski'
 author = 'Evan Pikulski'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.7'
+release = '0.1.8'
 
 
 # -- General configuration ---------------------------------------------------
